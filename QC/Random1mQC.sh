8dd4c36119e916d22e76adec489a478731e335ae
rkawaguc <rk350z@gmail.com>
2024-12-13 16:18:57 -0500

    Update Random1mQC.sh

--- QC/Random1mQC.sh
+++ QC/Random1mQC.sh
@@ -7,7 +7,7 @@
 
 sam=$1
 # get random 1M reads from Aligned.out.sam
-/share/apps/anaconda3/2020.07/envs/python-2.7/bin/python /coppolalabshares/amrf/RNAseq-tools/getRandomSamAllHits.py Aligned.out.sam 1000000
+/share/apps/anaconda3/2020.07/envs/python-2.7/bin/python getRandomSamAllHits.py Aligned.out.sam 1000000
 
 # convert sam to bam
 #/share/apps/samtools-1.9/bin/samtools view -Sb sample_any_1000000_Aligned.out.sam > temp.bam
